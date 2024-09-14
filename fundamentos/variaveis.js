0ba249e9485e0295f61e9fb49dd49cc133ac6d51
Kevin Teodoro <Kevin.teodoro7@outlook.com>
2024-09-13 21:34:51 -0300

    status das variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de constante e variáveis
+ * @author Kevin Teodoro
+ */
+
+console.clear()
+
+//variáveis
+let nome
+let idade
+let vip
+
+nome = "Kevin Teodoro"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = "24"
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
