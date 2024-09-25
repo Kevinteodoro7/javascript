86b4f45de65b2a2702dcffb747385347b4e4ac0a
Kevin Teodoro <Kevin.teodoro7@outlook.com>
2024-09-24 22:22:45 -0300

    atualizando

---
+++ fundamentos/precedencias.js
@@ -0,0 +1,13 @@
+/**
+ * Operadores aritiméticos
+ * Regra de precedência
+ */
+
+console.clear()
+
+console.log("Prioridade   |  simbologia")
+console.log("      1          |      ()")
+console.log("      2          |       *")
+console.log("      3          |       /")
+console.log("      4          |       +")
+console.log("      5          |       -")
---
+++ fundamentos/sintaxe1.js
@@ -0,0 +1,21 @@
+/**
+ * Sintaxe básica da linguagem JS
+ * @author Kevin Teodoro
+ */
+
+// Limpar a tela do console
+console.clear()
+
+// JS - Sentença e ; opcional
+console.log
+("Hello World");
+
+// concatenação (unir textos e variáveis)
+let nome = "Kevin Teodoro"
+console.log(`Nome: ${nome}`)
+console.log("Nome: " + nome) // não recomendado
+
+// arredondamento (casas decimais)
+let media = 5.789
+console.log(`Média: ${media.toFixed(1)}`)
+
