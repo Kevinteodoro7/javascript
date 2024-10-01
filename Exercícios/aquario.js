fd4c368c6d7070793dc9f0cbe21adb8b238158b9
Kevin Teodoro <Kevin.teodoro7@outlook.com>
2024-10-01 20:24:13 -0300

    Desafio do cálculo do volume do aquário

---
+++ Exercícios/aquario.js
@@ -0,0 +1,20 @@
+/**
+ * Cálculo do volume do aquário
+ * @author Kevin Teodoro
+ */
+
+const input = require ('readline-sync')
+
+// Variáveis
+let media, num1, num2, num3
+
+console.clear()
+console.log("cálculo do volume do aquário")
+
+// Entrada
+media = (num1 + num2 + num3) / 3
+
+// Saída
+media
+
+
