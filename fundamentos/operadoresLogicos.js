2498237130de8ee836ec77e03537fb426a1bcb1d
Kevin Teodoro <Kevin.teodoro7@outlook.com>
2024-10-10 22:16:12 -0300

    lista de exercicios

---
+++ fundamentos/operadoresLogicos.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores Lógicos (AND OR NOT)
+ * @author Kevin Teodoro
+ */
+
+console.clear()
+
+console.log("Operador lógico    |     Simbologia")
+console.log("             AND   |        &&     ")
+console.log("             OR    |        ||     ")
+console.log("             NOT   |         !     ")
